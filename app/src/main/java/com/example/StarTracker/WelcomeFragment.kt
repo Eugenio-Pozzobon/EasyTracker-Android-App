4de3060c3b83bd004b19ce44b30a8732be93d98b
Eugenio Pozzobon <eugeniopp00@gmail.com>
2021-04-24 10:50:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/StarTracker/WelcomeFragment.kt
@@ -0,0 +1,52 @@
+package com.example.StarTracker
+
+import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.StarTracker.databinding.FragmentWelcomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WelcomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+
+        binding.button.setOnClickListener{ v: View ->
+            val toast = Toast.makeText(getContext(), "GO", Toast.LENGTH_SHORT)
+            toast.show()
+            v.findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToCurrentProfileFragment())
+        }
+
+        setHasOptionsMenu(true)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+
+}
+
