a6405b3a35b8e26f709263b88225850f29ddd099
Eugenio Pozzobon <eugeniopp00@gmail.com>
2021-05-04 14:53:59 -0300

    Hide Keyboard when user touch outside

--- app/src/main/java/com/example/startracker/MainActivity.kt
+++ app/src/main/java/com/example/startracker/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.startracker
 
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
@@ -13,6 +18,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.startracker.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -99,5 +105,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    //Hide Keyboard when user touch outside
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val view: View? = currentFocus
+        if (view != null && (ev.action == MotionEvent.ACTION_UP || ev.action == MotionEvent.ACTION_MOVE) && view is EditText
+        ) {
+            val scrcoords = IntArray(2)
+            view.getLocationOnScreen(scrcoords)
+            val x: Float = ev.rawX + view.getLeft() - scrcoords[0]
+            val y: Float = ev.rawY + view.getTop() - scrcoords[1]
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom()) (this.getSystemService(
+                Context.INPUT_METHOD_SERVICE
+            ) as InputMethodManager).hideSoftInputFromWindow(
+                this.window.decorView.applicationWindowToken, 0
+            )
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
